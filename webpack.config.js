90807101f8aa430b78c18c43234c4806d4bc4428
sashazq@gmail.com <sashazq@gmail.com>
2017-02-11 20:41:49 +0200

    added bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 var config = {
     entry: './main.js',
 
@@ -7,12 +9,16 @@ var config = {
         filename: 'bundle.js',
     },
 
+    resolve: {
+        modulesDirectories: ['node_modules']
+    },
+
     devServer: {
         inline: true,
         port: 8080
     },
 
-    devtool: "#source-map",
+    // devtool: "#source-map",
 
     module: {
         loaders: [
@@ -24,13 +30,36 @@ var config = {
                 query: {
                     presets: ['es2015', 'react']
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file-loader',
             }
-        ]
+        ],
     },
-    plugin:[
+    plugins:[
         new HtmlWebpackPlugin({
             template: './index.html'
-        })
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.DedupePlugin(), //dedupe similar code
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            },
+        }), //minify everything
+        new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks
+        new ExtractTextPlugin('bundle.css')
     ]
 }
 
